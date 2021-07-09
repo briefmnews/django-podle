0be752df8fd645b799277296e00db2ce209abde8
Thomas <thomas.carbillet@gmail.com>
2021-07-09 12:30:05 +0200

    feat: add handle_rss_feeds
    this would create / delete rss feeds when adding / removing users from a group defined in PODLE_RSS_FEED_GROUP_NAME

--- podle/signals/handlers.py
+++ podle/signals/handlers.py
@@ -1,13 +1,16 @@
 import logging
 
 from django.conf import settings
-from django.db.models.signals import pre_save, pre_delete, post_save
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.db.models.signals import m2m_changed, pre_save, pre_delete, post_save
 from django.dispatch import receiver
 
 from ..models import Dictionary, RssFeed
 from ..podle import PodleHelper
 
 logger = logging.getLogger(__name__)
+User = get_user_model()
 
 
 @receiver(pre_save, sender=Dictionary)
@@ -52,6 +55,29 @@ def create_rss_feed(sender, instance, created, *args, **kwargs):
         instance.save()
 
         logger.info(response)
+
+
+@receiver(signal=m2m_changed, sender=User.groups.through)
+def handle_rss_feeds(instance, action, reverse, model, pk_set, using, *args, **kwargs):
+    """Create or delete private rss feeds when adding users to the group defined in PODLE_RSS_FEED_GROUP_NAME"""
+
+    # signal can have User (reverse=False) or Group (reverse=True) as sender
+    group_pks = [instance.pk] if reverse else pk_set
+
+    if action == "post_add" or action == "post_remove":
+        if not Group.objects.filter(
+            pk__in=group_pks, name=settings.PODLE_RSS_FEED_GROUP_NAME
+        ).exists():
+            return False
+
+        users_pks = pk_set if reverse else [instance.pk]
+
+        if action == "post_add":
+            for user_pk in users_pks:
+                RssFeed.objects.get_or_create(user_id=user_pk)
+        else:
+            rss_feeds = RssFeed.objects.filter(user_id__in=users_pks)
+            rss_feeds.delete()
 
 
 @receiver(pre_delete, sender=RssFeed)
